0b6567538feb73317422a6975b13974838db5da1
anonimoper <anonimoper12345@gmail.com>
2018-01-22 22:42:03 +0100

    Proyecto Shop. Estado inicial del proyecto con bootstrap y firebase configurado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyA9TQE7LXtjBBAY4FL6ZAF3w_zQN51FJ6w",
+        authDomain: "proyectoshop-8f138.firebaseapp.com",
+        databaseURL: "https://proyectoshop-8f138.firebaseio.com",
+        projectId: "proyectoshop-8f138",
+        storageBucket: "proyectoshop-8f138.appspot.com",
+        messagingSenderId: "468682975696"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA9TQE7LXtjBBAY4FL6ZAF3w_zQN51FJ6w",
+    authDomain: "proyectoshop-8f138.firebaseapp.com",
+    databaseURL: "https://proyectoshop-8f138.firebaseio.com",
+    projectId: "proyectoshop-8f138",
+    storageBucket: "proyectoshop-8f138.appspot.com",
+    messagingSenderId: "468682975696"
+}
 };
